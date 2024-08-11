e810351034dfacbbae14f28feebafdb0ff3fe6ca
Shivani Rajput <shivanirajpoot998@gmail.com>
2024-08-11 23:41:20 +0530

    day 28

---
+++ intersectionoftwolinkedlist.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *tempA = headA, *tempB = headB;
+    while (tempA != tempB) { 
+        tempA = tempA ? tempA->next : headB;
+        tempB = tempB ? tempB->next : headA;
+    }
+    return tempA;
+}
+};
